65cc7403557dd88bfb8d6d747e2048f811cf4926
Rex Cardan <rexcardan@gmail.com>
2024-05-21 08:30:56 -0500

    Add project files.

---
+++ Template.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAPM.SS._2024_CodeFlows
+{
+    public class Template
+    {
+        public string Generate()
+        {
+            return "Hello";
+        }
+    }
+}
